d0b100ee484f2eff9e21720b46b4de914cc3f1fb
Sebastian <hawaiilive@outlook.com>
2023-10-11 22:46:08 +0200

    contact

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,6 +6,26 @@ function Footer() {
     <footer>
       <h3>Enskilda Killer </h3>
       <p>Just nu byggs denna hemsida om</p>
+      <h3 className="text-lg text-black font-semibold">Contacts</h3>
+      <div className="flex flex-row gap-12">
+        <Link target="_blank" href={"mailto:hawaiilive@outlook.com"}>
+          Website: <p className="text-blue-200">hawaiilive@outlook.com</p>
+        </Link>
+        <Link target="_blank" href={"mailto:hawaiilive@outlook.com"}>
+          Killer:{" "}
+          <p className="text-blue-200">
+            neg_enskildakiller@nykopingsenskilda.com
+          </p>
+        </Link>
+        <Link
+          target="_blank"
+          href={
+            "https://github.com/Hawaii66/enskilda-killer/blob/master/README.md"
+          }
+        >
+          Github: <p className="text-blue-200">För dig som vill se allt</p>
+        </Link>
+      </div>
     </footer>
   );
 }
