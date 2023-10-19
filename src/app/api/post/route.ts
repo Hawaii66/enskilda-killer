acffeb29d88b60351610842b7a64fbfe75760401
Sebastian <hawaiilive@outlook.com>
2023-10-19 20:58:30 +0200

    latest post in profile

---
+++ src/app/api/post/route.ts
@@ -0,0 +1,33 @@
+import { emailToId } from "@/functions/emailToId";
+import { supabase } from "@/functions/supabase";
+import { VerifyWithEmail } from "@/functions/verifyToken";
+import { Post } from "@/interfaces/Post";
+import { NextRequest, NextResponse } from "next/server";
+
+export const GET = async (request: NextRequest) => {
+  const email = await VerifyWithEmail(request);
+  const id = await emailToId(email);
+  if (!email || !id) return NextResponse.json({}, { status: 400 });
+
+  const { data } = await supabase()
+    .from("posts")
+    .select("*")
+    .order("created_at", { ascending: false })
+    .limit(1)
+    .single();
+
+  if (!data) {
+    return NextResponse.json({}, { status: 400 });
+  }
+
+  const post: Post = {
+    createdAt: new Date(data.created_at).getTime(),
+    header: data.header,
+    id: data.id,
+    image: data.image,
+    miniheader: data.miniheader,
+    text: data.text,
+  };
+
+  return NextResponse.json(post);
+};
--- src/app/profil/page.tsx
+++ src/app/profil/page.tsx
@@ -7,8 +7,11 @@ import MeRenderer from "@/components/Profile/Default/MeRenderer";
 import SignUp from "@/components/Profile/NotSignedUp/SignUp";
 import ToLate from "@/components/Profile/NotSignedUp/ToLate";
 import Top from "@/components/Top";
+import PostRenderer from "@/components/blogg/PostRenderer";
+import { Separator } from "@/components/ui/separator";
 import { GameStateContext } from "@/contexts/GameStateContext";
 import { useApi } from "@/hooks/useApi";
+import { Post } from "@/interfaces/Post";
 import React, { useContext, useEffect, useState } from "react";
 
 export const revalidate = 0;
@@ -16,6 +19,7 @@ export const revalidate = 0;
 function Page() {
   const [exists, setExists] = useState(false);
   const [loading, setLoading] = useState(true);
+  const [latestPost, setLatestPost] = useState<Post>();
 
   const gameState = useContext(GameStateContext);
 
@@ -29,8 +33,16 @@ function Page() {
     }
   };
 
+  const fetchLatestPost = async () => {
+    const response = await apiFetch("/api/post", { method: "GET" });
+    if (response.status === 200) {
+      setLatestPost(await response.json());
+    }
+  };
+
   useEffect(() => {
     fetchExists();
+    fetchLatestPost();
   }, []);
 
   if (loading)
@@ -54,7 +66,14 @@ function Page() {
       <Top text="Profil" />
       <div className="w-full flex justify-center items-center">
         <div className="2xl:w-1/3 lg:w-2/3 w-11/12 flex justify-center items-center gap-8 pt-8 flex-col">
+          {latestPost && (
+            <div className="w-full">
+              <PostRenderer post={latestPost} />
+            </div>
+          )}
+          <Separator className="my-4" />
           <MeRenderer />
+
           <Circle />
           <Kills />
           <Litigations />
