3419eeab3bd8d88919951ac810ec769a8d90a122
Sebastian <hawaiilive@outlook.com>
2023-10-19 21:10:02 +0200

    Footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,8 +1,34 @@
 import Link from "next/link";
 import React from "react";
+import { Separator } from "./ui/separator";
 
 function Footer() {
-  return <footer></footer>;
+  return (
+    <footer className="mb-16 mt-12">
+      <Separator className="mb-4" />
+      <p>
+        Kontakt killer:{" "}
+        <Link
+          className="text-blue-300 tracking-wider underline"
+          href={"https://www.instagram.com/enskildakaren/"}
+          target="_blank"
+        >
+          EnskildaKåren
+        </Link>{" "}
+        (Instagram)
+      </p>
+      <p>
+        Kontakt hemsidan:{" "}
+        <Link
+          className="text-blue-300 tracking-wider underline"
+          href={"mailto:hawaiilive@outlook.com"}
+        >
+          hawaiilive@outlook.com
+        </Link>{" "}
+        (Sebastian Ahlman Na21B)
+      </p>
+    </footer>
+  );
 }
 
 export default Footer;
