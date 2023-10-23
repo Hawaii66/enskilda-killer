fd363630963fa4c05ff0029036fa054408d8309a
Sebastian Ahlman <hawaiilive@outlook.com>
2023-10-23 16:55:05 +0200

    script

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,6 +23,13 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1555847445754750"
+          crossOrigin="anonymous"
+        ></script>
+      </head>
       <AxiomWebVitals />
       <ClerkProvider>
         <GameStateWrapper>
