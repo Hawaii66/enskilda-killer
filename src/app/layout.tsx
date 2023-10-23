9175a78ed004a2677d9c40484da1dc95d3df9e9a
Sebastian Ahlman <hawaiilive@outlook.com>
2023-10-23 16:53:01 +0200

    tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,6 +23,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <meta name="google-adsense-account" content="ca-pub-1555847445754750" />
+      </head>
       <AxiomWebVitals />
       <ClerkProvider>
         <GameStateWrapper>
