a8b00bdb9f476a99d2276c8eff1931e168568324
Sebastian <hawaiilive@outlook.com>
2023-11-05 15:47:53 +0100

    better to late

--- src/components/Profile/NotSignedUp/ToLate.tsx
+++ src/components/Profile/NotSignedUp/ToLate.tsx
@@ -1,9 +1,36 @@
+"use client";
+
 import Top from "@/components/Top";
 import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Separator } from "@/components/ui/separator";
+import { useApi } from "@/hooks/useApi";
+import { useBasicToast } from "@/hooks/useBasicToast";
 import { SignOutButton } from "@clerk/nextjs";
-import React from "react";
+import Link from "next/link";
+import React, { useState } from "react";
 
 function ToLate() {
+  const [email, setEmail] = useState("");
+
+  const apiFetch = useApi();
+  const { toast, toastSave } = useBasicToast();
+
+  const fetchEmail = async () => {
+    const response = await apiFetch("/api/user/email", { method: "GET" });
+    if (response.status === 200) {
+      setEmail((await response.json()).email);
+    } else {
+      toast("Kunde inte hitta din email");
+    }
+  };
+
   return (
     <div>
       <Top text="Profil" />
@@ -14,9 +41,29 @@ function ToLate() {
         <p className="text-md text-gray-500 font-semibold">
           Du är inte med i Killer
         </p>
+        <p>Du är inloggad som: {email}</p>
         <SignOutButton>
           <Button variant={"outline"}>Logga ut</Button>
         </SignOutButton>
+        <Separator />
+
+        <Card>
+          <CardHeader>
+            <CardTitle>Fel email?</CardTitle>
+            <CardDescription>
+              Testa att först logga ut från microsoft
+            </CardDescription>
+          </CardHeader>
+          <CardContent className="pt-4 gap-2 flex flex-col">
+            <p>
+              Fel email? Logga ut från microsoft först uppe till höger på
+              länken:
+            </p>
+            <Link target="_blank" href="https://account.microsoft.com/">
+              <Button variant={"outline"}>Logga ut (Microsoft)</Button>
+            </Link>
+          </CardContent>
+        </Card>
       </div>
     </div>
   );
