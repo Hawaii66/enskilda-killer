3890be824d58044d5e523fc1961877b4b16c6c89
Sebastian Ahlman <hawaiilive@outlook.com>
2023-11-08 08:18:22 +0100

    fix: admin insert bug

--- src/app/api/admin/save/admins/route.ts
+++ src/app/api/admin/save/admins/route.ts
@@ -6,12 +6,12 @@ export const POST = async (request: NextRequest) => {
   const isAdmin = await checkIsAdmin(request);
   if (!isAdmin) return NextResponse.json({}, { status: 404 });
 
-  var admins: string[] = await request.json();
+  var admins: { email: string; name: string }[] = await request.json();
 
   await supabase().from("admins").delete().neq("id", -1);
   await supabase()
     .from("admins")
-    .insert(admins.map((i) => ({ email: i })));
+    .insert(admins.map((i) => ({ email: i.email, name: i.name })));
 
   return NextResponse.json({});
 };
