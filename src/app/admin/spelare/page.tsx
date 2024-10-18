7c7f7c607fe5714954c9d0312d5c5ecce6f85bdc
Sebastian <hawaiilive@outlook.com>
2024-10-18 18:20:10 +0200

    fix anoying bug

--- src/app/admin/spelare/page.tsx
+++ src/app/admin/spelare/page.tsx
@@ -2,17 +2,37 @@ import List from "@/components/admin/players/List";
 import { GetUsers } from "@/functions/admin/getPlayers";
 import React from "react";
 import { clerkClient } from "@clerk/nextjs";
+import { User } from "@clerk/nextjs/server";
 
 export const revalidate = 0;
 
 async function Page() {
   const users = await GetUsers();
-  //Todo: implement "pagination" to get all if above 500 in the future
-  const clerkUsers = await clerkClient.users.getUserList({ limit: 500 });
+  let clerkUsers: User[][] = [];
+  let page = 0;
+  while (true) {
+    let newUsers = await clerkClient.users.getUserList({
+      limit: 200,
+      offset: page * 200,
+    });
+
+    page += 1;
+    if (newUsers.length === 0) {
+      break;
+    }
+    clerkUsers.push(newUsers);
+  }
 
   return (
     <List
-      clerks={clerkUsers.map((i) => i.emailAddresses[0].emailAddress)}
+      clerks={clerkUsers
+        .flat()
+        .filter(
+          (i) =>
+            i.emailAddresses.length !== 0 &&
+            i.emailAddresses[0].emailAddress !== ""
+        )
+        .map((i) => i.emailAddresses[0].emailAddress)}
       users={users}
     />
   );
