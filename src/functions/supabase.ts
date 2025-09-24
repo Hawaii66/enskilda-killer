4e3d07fd0b47be96fb8aeb716818053e5e313adb
hawaiidev <hawaiilive@outlook.com>
2025-09-24 19:00:21 +0200

    reuse supabase client

--- src/functions/supabase.ts
+++ src/functions/supabase.ts
@@ -1,8 +1,22 @@
 import { Database } from "@/interfaces/database";
-import { createClient } from "@supabase/supabase-js";
+import { createClient, SupabaseClient } from "@supabase/supabase-js";
 
-export const supabase = () =>
-  createClient<Database>(
+const configureSupabase = () => {
+  _supabase = createClient<Database>(
     process.env.NEXT_PUBLIC_SUPABASE_URL || "",
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || ""
   );
+  return _supabase;
+};
+let _supabase: SupabaseClient<Database> | null = null;
+
+export const supabase = () => {
+  if (_supabase) {
+    console.log(
+      "reusing supabase - ---------------------------------------------------"
+    );
+    return _supabase;
+  }
+
+  return configureSupabase();
+};
